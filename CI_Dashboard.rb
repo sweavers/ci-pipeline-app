2e6151f0cc940017e3f0ab14269f1f781836cc44
Sebastian Weavers <sebastianweavers@localhost.localdomain>
2015-05-15 17:24:09 +0100

    Week view is now working

--- CI_Dashboard.rb
+++ CI_Dashboard.rb
@@ -165,135 +165,165 @@ end
 
 
 get '/home_week_view' do
-	if params["arrow_value"].nil? || params["arrow_value"].empty?
-		puts 'WARNING: No year was entered'
-		@arrow_value = 'no_value'
-	else
-		@arrow_value = params["arrow_value"]
-	end
-
 	time_data_obj = TimeData.new
+	monday = ''
+	month = ''
+	year = ''
 
 	if params["current_monday_month_year"].nil? || params["current_monday_month_year"].empty?
 		puts 'WARNING: No current_monday_month_year was entered'
 		current_time = Time.now
 		weekday_number = current_time.strftime("%w")
-		@current_day_month_year = current_time.strftime("%d%m%Y")
-		weekday = current_time.strftime("%d")
+		current_day = current_time.strftime("%d")
+		current_month = current_time.strftime("%m")
+		current_year = current_time.strftime("%Y")
 		if weekday_number.to_i == 1
-			monday_month_year = current_time.strftime("%d%m%Y")
-			@current_monday_month_year = monday_month_year.to_s
+			@current_monday_month_year = @current_day_month_year.to_s
 		else
-			current_month = @current_day_month_year[2..3].to_i
-			current_year = @current_day_month_year[4..7].to_i
-
-			#month_year = current_time.strftime("%m%Y")
 			difference = weekday_number.to_i - 1
-			weekday = weekday.to_i - difference
-			if weekday < 0
+			monday = current_day.to_i - difference
+			if monday < 0
 				previous_month = current_month - 1
-				previous_year = current_year
+
 				if previous_month == 0
 					previous_year = previous_year - 1
 					previous_month = 12
+					lastday_of_month = time_data_obj.days_in_month(previous_year, previous_month)
+				else
+					lastday_of_month = time_data_obj.days_in_month(current_year, previous_month)
 				end
 
-				lastday_of_month = time_data_obj.days_in_month(previous_year, previous_month)
-
-				weekday = lastday_of_month.to_i - difference
-				@current_monday_month_year = weekday.to_s + current_month.to_s + current_year.to_s
+				monday = lastday_of_month.to_i - difference
+				@current_monday_month_year = monday.to_s + current_month.to_s + current_year.to_s
 			else
-				weekday = weekday.to_i - difference
-        if weekday.to_s.length == 1
-          weekday = "0" + weekday.to_s
+        if monday.to_s.length == 1
+					monday = "0" + monday.to_s
         end
         if current_month.to_s.length == 1
           current_month = "0" + current_month.to_s
         end
-				@current_monday_month_year = weekday.to_s + current_month.to_s + current_year.to_s
+				@current_monday_month_year = monday.to_s + current_month.to_s + current_year.to_s
 			end
-			puts '}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}'
-			puts current_month.to_s + current_year.to_s
-			puts '>>>>>>>>>> month_year <<<<<<<<<<<<<<'
-			puts difference
-			puts '>>>>>>>>>> difference <<<<<<<<<<<<<<'
-			puts weekday
-			puts '>>>>>>>>>> weekday <<<<<<<<<<<<<<'
 		end
 	else
 		@current_monday_month_year = params["current_monday_month_year"]
 	end
 
-	puts '+++++++++++++++++++++++++++++++'
-	puts @current_monday_month_year
-	puts '+++++++++++++++++++++++++++++++'
+	time_data_obj = TimeData.new
 
-	current_month = 0
-	current_year = 0
+	subsequent_month = 0
+	subsequent_year = 0
 	previous_month = 0
 	previous_year = 0
 	previous_month_days = []
 	subsequent_month_days = []
+	firstday_of_month = 'empty'
+	month_name = ''
 
-	current_monday = @current_monday_month_year[0..1].to_i
-	current_month = @current_monday_month_year[2..3].to_i
-	current_year = @current_monday_month_year[4..7].to_i
-
-	plus_six = current_monday+6
+	input_monday = @current_monday_month_year[0..1].to_i
+	input_month = @current_monday_month_year[2..3].to_i
+	input_year = @current_monday_month_year[4..7].to_i
 
 	@weekdays = {}
-  @weekday_array = []
+	@weekday_array = []
 	@weekday_names = []
 
-  @weekday_names.push("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+	@weekday_names.push("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
-  count = -1
-	(current_monday..plus_six).each do |day|
-		@weekdays[day] = "current"
-    count = count + 1
-    @weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+	number_of_days = time_data_obj.days_in_month(input_year, input_month)
+
+	if params["arrow_value"].nil? || params["arrow_value"].empty?
+		puts 'WARNING: No year was entered'
+		@arrow_value = 'no_value'
+
+		plus_six = input_monday+6
+		count = -1
+		(input_monday..plus_six).each do |day|
+			@weekdays[day] = "current"
+			count = count + 1
+			@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+		end
+	else
+		@arrow_value = params["arrow_value"]
+		plus_six = input_monday+6
+
+		if @arrow_value == "positive"
+			new_monday = input_monday + 7
+			new_month = input_month
+			new_year = input_year
+			subsequent_weekdays = new_monday + 6
+
+			if new_monday > number_of_days
+				new_month = input_month + 1
+				new_year = input_year
+				if new_month == 13
+					new_year = input_year + 1
+					new_month = 1
+				end
+				new_monday = new_monday - number_of_days
+				subsequent_weekdays = new_monday + 6
+				count = -1
+				(new_monday..subsequent_weekdays).each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+				firstday_of_month = Time.parse('01.' + new_month.to_s + '.' + new_year.to_s)
+				month_name = firstday_of_month.strftime("%B")
+
+			elsif subsequent_weekdays > number_of_days
+				difference = subsequent_weekdays - number_of_days
+				current_weekdays = (new_monday..number_of_days).to_a
+				subsequent_weekdays = (1..difference).to_a
+				current_weekdays.push(*subsequent_weekdays)
+				count = -1
+				current_weekdays.each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+
+			else
+				count = -1
+				(new_monday..subsequent_weekdays).each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+			end
+
+		elsif @arrow_value == "minus"
+			previous_month = input_month - 1
+			if previous_month == 0
+				previous_year = input_year - 1
+				previous_month = 12
+			end
+
+			count = -1
+			(input_monday..plus_six).each do |day|
+				@weekdays[day] = "current"
+				count = count + 1
+				@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+			end
+			firstday_of_month = Time.parse('01.' + input_month.to_s + '.' + input_year.to_s)
+			month_name = firstday_of_month.strftime("%B")
+		end
+
+		(new_monday.to_s.length == 1) ? (new_monday = '0' + new_monday.to_s) : (puts 'do nothing')
+		(new_month.to_s.length == 1) ? (new_month = '0' + new_month.to_s) : (puts 'do nothing')
+		@current_monday_month_year = new_monday.to_s + new_month.to_s + new_year.to_s
 	end
 
-  puts '[][][][][][][][][][]'
-  puts @weekday_array
-  puts '[][][][][][][][][][]  weekday_array'
-  puts current_month
-  puts '[][][][][][][][][][]  current_month'
-  puts @weekdays.to_s
-  puts '[][][][][][][][][][]  weekdays_hash'
-
-	if @arrow_value == "positive"
-		current_month = current_month + 1
-		if current_month == 13
-			current_year = current_year + 1
-			current_month = 1
-		end
-	elsif @arrow_value == "minus"
-		current_month = current_month - 1
-		if current_month == 0
-			current_year = current_year - 1
-			current_month = 12
-		end
+	if firstday_of_month == 'empty'
+		firstday_of_month = Time.parse('01.' + input_month.to_s + '.' + input_year.to_s)
+		month_name = firstday_of_month.strftime("%B")
 	end
 
-	subsequent_month_name = time_data_obj.subsequent_month_name(current_month)
-	previous_month_name = time_data_obj.previous_month_name(current_month)
-
-	puts '########################'
-	puts current_year
-	puts '--------------------------------'
-	puts current_month
-	puts '########################'
-
-	time_data_obj = TimeData.new
-
-	number_of_days = time_data_obj.days_in_month(current_year, current_month)
-
-	firstday_of_month = Time.parse('01.' + current_month.to_s + '.' + current_year.to_s)
-	month_name = firstday_of_month.strftime("%B")
+	previous_month_name = time_data_obj.previous_month_name(input_month)
+	subsequent_month_name = time_data_obj.subsequent_month_name(input_month)
 
 	@current_monday_month_year
-	@month_year = month_name + ' ' + current_year.to_s
+	@month_year = month_name + ' ' + input_year.to_s
   @weekday_array
 	@weekdays
 	@previous_month = previous_month_name.to_s
@@ -403,5 +433,81 @@ end
 					<div class="calendar_text" align="right"><%=time_slot%></div>
 				</div>
 			<% end %>
+			<option id="1900" value="1900">19:00</option>
+			<option id="2000" value="2000">20:00</option>
+			<option id="2100" value="2100">21:00</option>
+			<option id="2200" value="2200">22:00</option>
+			<option id="2300" value="2300">23:00</option>
+			<option id="0000" value="0000">00:00</option>
+			<option id="0100" value="0100">01:00</option>
+			<option id="0200" value="0200">02:00</option>
+			<option id="0300" value="0300">03:00</option>
+=end
+
+
+=begin
+<label class="page1">Start Time</label>
+<div class="tooltips" title="Please select the start_time that the customer will primarily be served from">
+    <select id="start_time" name="start_time" placeholder="Phantasyland">
+        <option></option>
+        <option>19:00</option>
+        <option>20:00</option>
+        <option>21:00</option>
+        <option>22:00</option>
+        <option>23:00</option>
+        <option>00:00</option>
+        <option>01:00</option>
+        <option>02:00</option>
+        <option>03:00</option>
+    </select>
+</div>
+<br />
+<br />
+<label class="page1">End Time</label>
+<div class="tooltips" title="Please select the city that the customer is primarily to be served from.">
+    <select id="end_time" name="end_time" placeholder="Anycity"></select>
+</div>
+=end
+
+=begin
+jQuery(function($) {
+    var end_times = {
+        '19:00': ["20:00", "21:00", "22:00", "23:00", "00:00", "01:00", "02:00", "03:00", "04:00"],
+        '20:00': ["21:00", "22:00", "23:00", "00:00", "01:00", "02:00", "03:00", "04:00"],
+        '21:00': ["22:00", "23:00", "00:00", "01:00", "02:00", "03:00", "04:00"],
+        '22:00': ["23:00", "00:00", "01:00", "02:00", "03:00", "04:00"],
+        '23:00': ["00:00", "01:00", "02:00", "03:00", "04:00"],
+        '00:00': ["01:00", "02:00", "03:00", "04:00"],
+        '01:00': ["02:00", "03:00", "04:00"],
+        '02:00': ["03:00", "04:00"],
+        '03:00': ["04:00"]
+    }
+
+    var $end_times = $('#end_time');
+    $('#start_time').change(function () {
+        var start_time = $(this).val(), lcns = end_times[start_time] || [];
+
+        var hours = start_time.substring(0, 2);
+        var new_count = 0
+        var html = '';
+        for (var counter = 1; counter < 5; counter++) {
+            var additional_hours = (parseInt(hours) + counter).toString()
+            if (additional_hours.length == 1) {
+                var additional_hours = '0' + additional_hours + ':00';
+            }
+            else {
+                var additional_hours = additional_hours + ':00';
+            }
+            if (additional_hours == "24:00") {
+                var additional_hours = "00:00";
+                var hours = 0-counter;
+            }
+          var html = html + '<option value="'+ additional_hours + '">' + additional_hours + '</option>'
+        }
+
+
+        $end_times.html(html)
+    });
+});
 
 =end
