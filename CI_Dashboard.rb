dfce177c2f7f6b3f818687f7f9da455ca05a5057
Sebastian Weavers <sebastianweavers@localhost.localdomain>
2015-05-18 11:56:10 +0100

    Week view calendar now fully fixed

--- CI_Dashboard.rb
+++ CI_Dashboard.rb
@@ -50,7 +50,7 @@ class TimeData
 	end
 
 	def previous_month_name(month)
-		previous_month = month - 1
+		previous_month = month.to_i - 1
 		if previous_month == 0
 			previous_month = 12
 		end
@@ -59,7 +59,7 @@ class TimeData
 	end
 
 	def subsequent_month_name(month)
-		subsequent_month = month + 1
+		subsequent_month = month.to_i + 1
 		if subsequent_month == 13
 			subsequent_month = 1
 		end
@@ -178,7 +178,7 @@ get '/home_week_view' do
 		current_month = current_time.strftime("%m")
 		current_year = current_time.strftime("%Y")
 		if weekday_number.to_i == 1
-			@current_monday_month_year = @current_day_month_year.to_s
+			@current_monday_month_year = current_day.to_s + current_month.to_s + current_year.to_s
 		else
 			difference = weekday_number.to_i - 1
 			monday = current_day.to_i - difference
@@ -233,9 +233,7 @@ get '/home_week_view' do
 	number_of_days = time_data_obj.days_in_month(input_year, input_month)
 
 	if params["arrow_value"].nil? || params["arrow_value"].empty?
-		puts 'WARNING: No year was entered'
 		@arrow_value = 'no_value'
-
 		plus_six = input_monday+6
 		count = -1
 		(input_monday..plus_six).each do |day|
@@ -255,7 +253,6 @@ get '/home_week_view' do
 
 			if new_monday > number_of_days
 				new_month = input_month + 1
-				new_year = input_year
 				if new_month == 13
 					new_year = input_year + 1
 					new_month = 1
@@ -293,22 +290,43 @@ get '/home_week_view' do
 			end
 
 		elsif @arrow_value == "minus"
-			previous_month = input_month - 1
-			if previous_month == 0
-				previous_year = input_year - 1
-				previous_month = 12
+			new_monday = input_monday - 7
+			new_month = input_month
+			new_year = input_year
+			subsequent_weekdays = new_monday + 6
+
+			if new_monday <= 0
+				new_month = input_month - 1
+				if new_month == 0
+					new_year = input_year - 1
+					new_month = 12
+				end
+				previous_number_of_days = time_data_obj.days_in_month(new_year, new_month)
+				difference = new_monday + 6
+				new_monday = new_monday + previous_number_of_days
+
+				current_weekdays = (new_monday..previous_number_of_days).to_a
+				subsequent_weekdays = (1..difference).to_a
+				current_weekdays.push(*subsequent_weekdays)
+				count = -1
+				current_weekdays.each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+
+				firstday_of_month = Time.parse('01.' + new_month.to_s + '.' + new_year.to_s)
+				month_name = firstday_of_month.strftime("%B")
+			else
+				count = -1
+				(new_monday..subsequent_weekdays).each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
 			end
 
-			count = -1
-			(input_monday..plus_six).each do |day|
-				@weekdays[day] = "current"
-				count = count + 1
-				@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
-			end
-			firstday_of_month = Time.parse('01.' + input_month.to_s + '.' + input_year.to_s)
-			month_name = firstday_of_month.strftime("%B")
 		end
-
 		(new_monday.to_s.length == 1) ? (new_monday = '0' + new_monday.to_s) : (puts 'do nothing')
 		(new_month.to_s.length == 1) ? (new_month = '0' + new_month.to_s) : (puts 'do nothing')
 		@current_monday_month_year = new_monday.to_s + new_month.to_s + new_year.to_s
@@ -319,8 +337,35 @@ get '/home_week_view' do
 		month_name = firstday_of_month.strftime("%B")
 	end
 
-	previous_month_name = time_data_obj.previous_month_name(input_month)
-	subsequent_month_name = time_data_obj.subsequent_month_name(input_month)
+	if new_monday.nil?
+		subsequent_monday = input_monday.to_i + 7
+		previous_monday = input_monday.to_i - 7
+	else
+		subsequent_monday = new_monday.to_i + 7
+		previous_monday = new_monday.to_i - 7
+	end
+
+	if subsequent_monday > number_of_days
+		if new_month.nil?
+			previous_month_name = '<'
+			subsequent_month_name = time_data_obj.subsequent_month_name(input_month)
+		else
+			previous_month_name = '<'
+			subsequent_month_name = time_data_obj.subsequent_month_name(new_month)
+		end
+	elsif previous_monday <= 0
+		if new_month.nil?
+			previous_month_name = time_data_obj.previous_month_name(input_month)
+			subsequent_month_name = '>'
+		else
+			previous_month_name = time_data_obj.previous_month_name(new_month)
+			subsequent_month_name = '>'
+		end
+	else
+		previous_month_name = '<'
+		subsequent_month_name = '>'
+	end
+
 
 	@current_monday_month_year
 	@month_year = month_name + ' ' + input_year.to_s
@@ -338,15 +383,214 @@ get '/new_lcr' do
 end
 
 get '/new_deployment' do
-	@weekdays = {}
+	time_data_obj = TimeData.new
+	monday = ''
+	month = ''
+	year = ''
 
-	count = -1
-	(1..7).each do |day|
-		@weekdays[day] = "current"
-		count = count + 1
+	if params["current_monday_month_year"].nil? || params["current_monday_month_year"].empty?
+		puts 'WARNING: No current_monday_month_year was entered'
+		current_time = Time.now
+		weekday_number = current_time.strftime("%w")
+		current_day = current_time.strftime("%d")
+		current_month = current_time.strftime("%m")
+		current_year = current_time.strftime("%Y")
+		if weekday_number.to_i == 1
+			@current_monday_month_year = current_day.to_s + current_month.to_s + current_year.to_s
+		else
+			difference = weekday_number.to_i - 1
+			monday = current_day.to_i - difference
+			if monday < 0
+				previous_month = current_month - 1
+
+				if previous_month == 0
+					previous_year = previous_year - 1
+					previous_month = 12
+					lastday_of_month = time_data_obj.days_in_month(previous_year, previous_month)
+				else
+					lastday_of_month = time_data_obj.days_in_month(current_year, previous_month)
+				end
+
+				monday = lastday_of_month.to_i - difference
+				@current_monday_month_year = monday.to_s + current_month.to_s + current_year.to_s
+			else
+				if monday.to_s.length == 1
+					monday = "0" + monday.to_s
+				end
+				if current_month.to_s.length == 1
+					current_month = "0" + current_month.to_s
+				end
+				@current_monday_month_year = monday.to_s + current_month.to_s + current_year.to_s
+			end
+		end
+	else
+		@current_monday_month_year = params["current_monday_month_year"]
 	end
 
+	time_data_obj = TimeData.new
+
+	subsequent_month = 0
+	subsequent_year = 0
+	previous_month = 0
+	previous_year = 0
+	previous_month_days = []
+	subsequent_month_days = []
+	firstday_of_month = 'empty'
+	month_name = ''
+
+	input_monday = @current_monday_month_year[0..1].to_i
+	input_month = @current_monday_month_year[2..3].to_i
+	input_year = @current_monday_month_year[4..7].to_i
+
+	@weekdays = {}
+	@weekday_array = []
+	@weekday_names = []
+
+	@weekday_names.push("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+
+	number_of_days = time_data_obj.days_in_month(input_year, input_month)
+
+	if params["arrow_value"].nil? || params["arrow_value"].empty?
+		@arrow_value = 'no_value'
+		plus_six = input_monday+6
+		count = -1
+		(input_monday..plus_six).each do |day|
+			@weekdays[day] = "current"
+			count = count + 1
+			@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+		end
+	else
+		@arrow_value = params["arrow_value"]
+		plus_six = input_monday+6
+
+		if @arrow_value == "positive"
+			new_monday = input_monday + 7
+			new_month = input_month
+			new_year = input_year
+			subsequent_weekdays = new_monday + 6
+
+			if new_monday > number_of_days
+				new_month = input_month + 1
+				if new_month == 13
+					new_year = input_year + 1
+					new_month = 1
+				end
+				new_monday = new_monday - number_of_days
+				subsequent_weekdays = new_monday + 6
+				count = -1
+				(new_monday..subsequent_weekdays).each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+				firstday_of_month = Time.parse('01.' + new_month.to_s + '.' + new_year.to_s)
+				month_name = firstday_of_month.strftime("%B")
+
+			elsif subsequent_weekdays > number_of_days
+				difference = subsequent_weekdays - number_of_days
+				current_weekdays = (new_monday..number_of_days).to_a
+				subsequent_weekdays = (1..difference).to_a
+				current_weekdays.push(*subsequent_weekdays)
+				count = -1
+				current_weekdays.each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+
+			else
+				count = -1
+				(new_monday..subsequent_weekdays).each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+			end
+
+		elsif @arrow_value == "minus"
+			new_monday = input_monday - 7
+			new_month = input_month
+			new_year = input_year
+			subsequent_weekdays = new_monday + 6
+
+			if new_monday <= 0
+				new_month = input_month - 1
+				if new_month == 0
+					new_year = input_year - 1
+					new_month = 12
+				end
+				previous_number_of_days = time_data_obj.days_in_month(new_year, new_month)
+				difference = new_monday + 6
+				new_monday = new_monday + previous_number_of_days
+
+				current_weekdays = (new_monday..previous_number_of_days).to_a
+				subsequent_weekdays = (1..difference).to_a
+				current_weekdays.push(*subsequent_weekdays)
+				count = -1
+				current_weekdays.each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+
+				firstday_of_month = Time.parse('01.' + new_month.to_s + '.' + new_year.to_s)
+				month_name = firstday_of_month.strftime("%B")
+			else
+				count = -1
+				(new_monday..subsequent_weekdays).each do |day|
+					@weekdays[day] = "current"
+					count = count + 1
+					@weekday_array.push(@weekday_names[count] + ", " + day.to_s)
+				end
+			end
+
+		end
+		(new_monday.to_s.length == 1) ? (new_monday = '0' + new_monday.to_s) : (puts 'do nothing')
+		(new_month.to_s.length == 1) ? (new_month = '0' + new_month.to_s) : (puts 'do nothing')
+		@current_monday_month_year = new_monday.to_s + new_month.to_s + new_year.to_s
+	end
+
+	if firstday_of_month == 'empty'
+		firstday_of_month = Time.parse('01.' + input_month.to_s + '.' + input_year.to_s)
+		month_name = firstday_of_month.strftime("%B")
+	end
+
+	if new_monday.nil?
+		subsequent_monday = input_monday.to_i + 7
+		previous_monday = input_monday.to_i - 7
+	else
+		subsequent_monday = new_monday.to_i + 7
+		previous_monday = new_monday.to_i - 7
+	end
+
+	if subsequent_monday > number_of_days
+		if new_month.nil?
+			previous_month_name = '<'
+			subsequent_month_name = time_data_obj.subsequent_month_name(input_month)
+		else
+			previous_month_name = '<'
+			subsequent_month_name = time_data_obj.subsequent_month_name(new_month)
+		end
+	elsif previous_monday <= 0
+		if new_month.nil?
+			previous_month_name = time_data_obj.previous_month_name(input_month)
+			subsequent_month_name = '>'
+		else
+			previous_month_name = time_data_obj.previous_month_name(new_month)
+			subsequent_month_name = '>'
+		end
+	else
+		previous_month_name = '<'
+		subsequent_month_name = '>'
+	end
+
+
+	@current_monday_month_year
+	@month_year = month_name + ' ' + input_year.to_s
+	@weekday_array
 	@weekdays
+	@previous_month = previous_month_name.to_s
+	@subsequent_month = subsequent_month_name.to_s
 	erb :dashboard_new_deployment
 end
 
